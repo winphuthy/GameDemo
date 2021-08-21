7679fe0df1fc9f85883de11cdfec08281bacef8e
Alexhahh <88976310+Alexhahh@users.noreply.github.com>
2021-08-21 20:49:19 +1000

    Add files via upload
    level system

---
+++ Level.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[System.Serializable]
+public class Level 
+{
+    public int currentLevel;
+    public int currentEXP;
+    public int ExpForNextLevel;
+
+    public Level(int currentLevel, int currentEXP)
+    {
+        this.currentLevel = currentLevel;
+        this.currentEXP = currentEXP;
+        SetExpForLevelup();
+
+    }
+
+    public void SetExpForLevelup()
+    {    
+        ExpForNextLevel = (int)Math.Floor(100*Math.Pow(1.25, currentLevel - 1));
+    }
+
+    public void AddEXP(int EXP)
+    {
+        currentEXP += EXP;
+        if(currentEXP - ExpForNextLevel >= 0)
+        {
+            LevelUp();
+        }
+
+        Debug.Log("Current Level is: " + currentLevel + " Current EXP is: " +currentEXP + " EXP needed to Next Level: " + ExpForNextLevel);
+    }
+
+    public void LevelUp()
+    {
+        currentEXP = currentEXP - ExpForNextLevel;
+        currentLevel++;
+        SetExpForLevelup();
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Player : MonoBehaviour
+{
+    public Level level;
+    void Start()
+    {
+        level = new Level(1,0);
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        level.AddEXP(100);
+    }
+
+}
